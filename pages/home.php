82f14bd881d26904de7b1123a79b624c9285c3c3
snyggen911 <alleboii96@gmail.com>
2016-08-12 17:32:03 +0200

    Add files via upload

---
+++ pages/home.php
@@ -0,0 +1,10 @@
+ Update field.<hr>This field will automatically update the content.
+ <br><hr>
+	
+<a href="?page=success" class="link btn btn-success">.btn btn-success</a>
+<a href="?page=warning" class="link btn btn-warning">.btn btn-warning</a>
+<a href="?page=danger" class="link btn btn-danger">.btn btn-danger</a>
+<a href="?page=info" class="link btn btn-info">.btn btn-info</a><br>
+
+<hr>
+<div id="show"> </div>
